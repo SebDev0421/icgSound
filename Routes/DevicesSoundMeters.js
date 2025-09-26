9ec1c59e57a3b0087a3cdcbf7d2c30f23b61be4b
Sebastian Guerrero <43431640+SebDev0421@users.noreply.github.com>
2025-09-26 09:59:34 -0500

    adding download data

--- Routes/DevicesSoundMeters.js
+++ Routes/DevicesSoundMeters.js
@@ -21,6 +21,49 @@ app.put('/lastData', async(req, res) => {
     
     res.json({message: 'ok', data: lastDoc})
 })
+
+
+function toCompactDate(isoString) {
+  const d = isoString;
+  console.log("day toCompactDate:", d.getDate());
+  const pad = (n) => n.toString().padStart(2, "0");
+  return (
+    d.getFullYear().toString() +
+    pad(d.getMonth() + 1) +
+    pad(d.getDate()+1) +
+    "000000"
+  );
+}
+
+app.post('/dayData', async(req, res) => {
+    const {id, date} = req.body
+    console.log("Request dayData for id:", id, "and date:", date);
+    await client.connect();
+    const db = client.db("proyecto1111");
+    const collection = db.collection(id);
+    const start = new Date(date);
+    const end = new Date(date);
+    end.setDate(end.getDate() + 1); // Sumar un día para incluir todo el día
+
+    //format date in database is 20250915000000
+
+    console.log("Fetching data from", start, "to", end);
+
+    const startFormatted = parseInt(toCompactDate(start));
+    const endFormatted = parseInt(toCompactDate(end));
+
+    console.log("Formatted dates:", startFormatted, endFormatted);
+    //section to fetch data between start and end
+
+    const docs = await collection
+        .find({ register: { $gte: startFormatted, $lt: endFormatted } })
+        .sort({ register: 1 }) // Ordenar por fecha ascendente
+        .toArray();
+
+    res.json({message: 'ok', data: docs})
+})
+
+
     
 
 module.exports = app
